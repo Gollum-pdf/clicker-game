25a36a11480e4d154c7b76988642b716a5bc1e33
Gollum.pdf <tomas.elliott@stpats.school.nz>
2025-07-31 10:15:32 +1200

    click work  now

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-document.getElementById("clicker").addEventListener("click",clickFunction);
-var score = 0;
-function clickFunction() {score++}
+let troops = 0;
+function click(){
+    troops++;
+}
+document.getElementById("sword").addEventListener("click", click);
+function main(){
+    document.getElementById("troops").innerHTML = ("Troops: " +troops);
+}
+setInterval(main, 1);
