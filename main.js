ffd591914a498369f9b91a9013d2f961f7e64c7d
Gollum.pdf <tomas.elliott@stpats.school.nz>
2025-08-05 11:15:48 +1200

    buy get there

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 let troops = 0;
+let tpc = 1;
+let cost = 10;
 function click(){
-    troops++;
+    troops += tpc;
 }
-document.getElementById("sword").addEventListener("click", click);
+document.getElementById("milk").addEventListener("click", click);
+
+function buy(){
+    if(troops>=cost) {
+        troops-=cost;
+    tpc++;
+    cost+=5;
+    } else {
+        alert("You cannot afford this :(");
+    }
+    
+}
+document.getElementById("buy").addEventListener("click", buy);
+
 function main(){
     document.getElementById("troops").innerHTML = ("Troops: " +troops);
 }
