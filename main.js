86482787a7454ee9df06d51a719334bd710996f9
Gollum.pdf <tomas.elliott@stpats.school.nz>
2025-08-05 12:40:31 +1200

    did more thing with buy

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let troops = 0;
 let tpc = 1;
 let cost = 10;
+let dtpc = 1;
 function click(){
     troops += tpc;
 }
@@ -9,8 +10,9 @@ document.getElementById("milk").addEventListener("click", click);
 function buy(){
     if(troops>=cost) {
         troops-=cost;
-    tpc++;
-    cost+=5;
+        tpc++;
+        cost+=15;
+        dtpc++;
     } else {
         alert("You cannot afford this :(");
     }
@@ -20,5 +22,7 @@ document.getElementById("buy").addEventListener("click", buy);
 
 function main(){
     document.getElementById("troops").innerHTML = ("Troops: " +troops);
+    document.getElementById("buy").innerHTML = ("Archer<br> +1 troop per click<br> Cost = " +cost);
+    document.getElementById("dtpc").innerHTML = ("Troops per click: " +dtpc);
 }
 setInterval(main, 1);
