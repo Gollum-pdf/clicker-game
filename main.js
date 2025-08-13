ddc8cd5c3e83a33a393e0803c137438276b2115f
Gollum.pdf <tomas.elliott@stpats.school.nz>
2025-08-14 10:15:31 +1200

    cast done

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 let troops = 0;
 let tpc = 1;
 let tps = 0;
-let costcav = 100;
 let upgrades = ["archer","cav","treb","ae","cast"];
 let upgradecost = [10, 100, 500, 1000, 5000];
-let upgradefunc = [function() {tpc++},function() {tps++}, function() {{tpc+=10}}];
-let upgradecostincrease = [15,50,500,5000,50000]
+let upgradefunc = [function() {tpc++},function() {tps++}, function() {tpc+=10}, function() {tps+=10}, function() {troops-=troops,tpc-=tpc,tps-=tps}];
+let upgradecostincrease = [15,50,500,5000,10000];
 let hover = false;
-let elIds = ["troops","archer","tpc","cav","tps", "treb"];
+let elIds = ["troops","archer","tpc","cav","tps", "treb", "ae", "cast"];
 function click(){
     troops += tpc;
 }
@@ -18,6 +17,8 @@ function main(){
     document.getElementById("cav").innerHTML = ("Cavalry<br> +1 troops per second<br> Cost = " + upgradecost[1]);
     document.getElementById("tps").innerHTML = ("Troops per Second: " + tps);
     document.getElementById("treb").innerHTML = ("Trebuchet<br> +10 troops per click<br> Cost = " + upgradecost[2]);
+    document.getElementById("ae").innerHTML = ("Armoured Elephant<br> +10 troops per second<br> Cost = " + upgradecost[3]);
+    document.getElementById("cast").innerHTML = ("Castle<br>The final upgrade, you need not anything more.<br> Cost = " + upgradecost[4]);
     for (let i=0; i<elIds.length; i++){
         try{
             if(document.getElementById(elIds[i]).matches(':hover')){
